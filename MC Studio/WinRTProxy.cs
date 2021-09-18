51c9ab4a6586a1822b4afd3764dcbbb67b63bf70
Lukas Kurz <vb.net@kurzweb.de>
2021-09-18 21:52:46 +0200

    Improvements to WinRTProxy (loading proplem still present)

--- MC Studio/WinRTProxy.cs
+++ MC Studio/WinRTProxy.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using FormsApplication = System.Windows.Forms.Application;
-using Windows.UI.Popups;
-using System.Diagnostics;
+﻿using Microsoft.VisualBasic.CompilerServices;
 using Microsoft.Web.WebView2.Core;
-using System.Dynamic;
-using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
-using Microsoft.VisualBasic.CompilerServices;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Windows.UI.Popups;
+using FormsApplication = System.Windows.Forms.Application;
 
 namespace MC_Studio
 {
@@ -19,7 +20,8 @@ namespace MC_Studio
             this.WebView = webview;
         }
 
-        public WinRTProxies.WindowsNSProxy Windows = new WinRTProxies.WindowsNSProxy();
+        public NamespaceProxy Windows = new NamespaceProxy("Windows");
+        //public WinRTProxies.WindowsNSProxy Windows = new WinRTProxies.WindowsNSProxy();
         public ObjectProxy Process = new ObjectProxy(System.Diagnostics.Process.GetCurrentProcess());
         public CoreWebView2 WebView;
     }
@@ -47,98 +49,236 @@ namespace MC_Studio
         }
     }
 
+    public class WinRTAssemblyManager
+    {
+        public static string WinMetdadataDir { get => "C:/Windows/System32/WinMetadata/"; }
+
+        private static HashSet<Assembly> _Assemblies;
+        public static Assembly[] Assemblies
+        {
+            get
+            {
+                if (_Assemblies == null)
+                {
+                    _Assemblies = new HashSet<Assembly>();
+                    foreach (string windmPath in Directory.GetFiles(WinMetdadataDir, "*.winmd"))
+                    {
+                        try
+                        {
+                            _Assemblies.Add(Assembly.Load(File.ReadAllBytes(windmPath)));
+                        }
+                        catch { }
+                    }
+                }
+                //var x = typeof(MessageDialog).Assembly; // Force load "Windows.UI.winmd"
+                //Debugger.Break();
+                //return AppDomain.CurrentDomain.GetAssemblies();
+                return _Assemblies.ToArray();
+            }
+        }
+
+        private static Dictionary<string, List<Type>> _Namespaces;
+        public static string[] Namespaces
+        {
+            get
+            {
+                if (_Namespaces == null)
+                {
+                    _Namespaces = new Dictionary<string, List<Type>>();
+                    foreach (Assembly assm in Assemblies)
+                    {
+                        foreach (Type type in assm.ExportedTypes)
+                        {
+                            List<Type> types = new List<Type>();
+                            if (_Namespaces.ContainsKey(type.Namespace))
+                                types = _Namespaces[type.Namespace];
+                            else
+                                _Namespaces.Add(type.Namespace, types);
+                            types.Add(type);
+                        }
+                    }
+                }
+                return _Namespaces.Keys.ToArray();
+            }
+        }
+
+        public static Type[] GetTypesInNamespcae(string @namespace)
+        {
+            if (!_Namespaces.ContainsKey(@namespace))
+                return new Type[] { };
+            return _Namespaces[@namespace].ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Exposes namespaces as nested objects for JS
+    /// </summary>
     [ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
     public class NamespaceProxy : IReflect
     {
-        public Assembly Assembly { get; private set; }
         public string Namespace { get; private set; }
-        public NamespaceProxy(Assembly assembly, string @namespace)
+        public NamespaceProxy(string @namespace)
         {
-            this.Assembly = assembly;
             this.Namespace = @namespace;
         }
 
-        public Type UnderlyingSystemType => typeof(Object);
+        public Type UnderlyingSystemType => typeof(object);
 
-        public FieldInfo GetField(string name, BindingFlags bindingAttr)
-        {
-            throw new NotImplementedException();
-        }
+        public FieldInfo GetField(string name, BindingFlags bindingAttr) => GetFields(bindingAttr).Where((x) => x.Name == name).FirstOrDefault();
 
         public FieldInfo[] GetFields(BindingFlags bindingAttr)
         {
-            return new[] { new FieldInfo("") };
+            var fields = WinRTAssemblyManager.Namespaces.AsParallel().Where((x) => (x.Contains(".") ? x.Substring(0, x.LastIndexOf(".")) : x) == this.Namespace).Select((x) => new NamespaceFieldInfo(x.Split('.').Last())).ToArray();
+            return fields;
         }
 
+        public MethodInfo GetMethod(string name, BindingFlags bindingAttr, Binder binder, Type[] types, ParameterModifier[] modifiers) => GetMethod(name, bindingAttr);
+
+        public MethodInfo GetMethod(string name, BindingFlags bindingAttr) => GetMethods(bindingAttr).Where((x) => x.Name == name).FirstOrDefault();
+
+        public MethodInfo[] GetMethods(BindingFlags bindingAttr)
+        {
+            return WinRTAssemblyManager.GetTypesInNamespcae(Namespace).Where((x) => x.GetConstructors().Length > 0).Select((x) => new ObjectConstructorMethodInfo(x, x.GetConstructors().First())).ToArray();
+        }
+
+        #region Not in use
         public MemberInfo[] GetMember(string name, BindingFlags bindingAttr) => new MemberInfo[] { };
 
         public MemberInfo[] GetMembers(BindingFlags bindingAttr) => new MemberInfo[] { };
-
-        public MethodInfo GetMethod(string name, BindingFlags bindingAttr, Binder binder, Type[] types, ParameterModifier[] modifiers) => null;
-
-        public MethodInfo GetMethod(string name, BindingFlags bindingAttr) => null;
-
-        public MethodInfo[] GetMethods(BindingFlags bindingAttr) => new MethodInfo[] { };
 
         public PropertyInfo[] GetProperties(BindingFlags bindingAttr) => new PropertyInfo[] { };
 
         public PropertyInfo GetProperty(string name, BindingFlags bindingAttr) => null;
 
         public PropertyInfo GetProperty(string name, BindingFlags bindingAttr, Binder binder, Type returnType, Type[] types, ParameterModifier[] modifiers) => null;
+        #endregion
 
         public object InvokeMember(string name, BindingFlags invokeAttr, Binder binder, object target, object[] args, ParameterModifier[] modifiers, CultureInfo culture, string[] namedParameters)
         {
-            throw new NotImplementedException();
+            if (invokeAttr.HasFlag(BindingFlags.GetField) || invokeAttr.HasFlag(BindingFlags.GetProperty))
+            {
+                FieldInfo field = GetField(name, invokeAttr);
+                if (field != null)
+                    return field.GetValue(this);
+            }
+
+            MethodInfo method = GetMethod(name, invokeAttr);
+            if (method != null)
+            {
+                if (!invokeAttr.HasFlag(BindingFlags.InvokeMethod))
+                    throw new MissingMethodException();
+                return method.Invoke(this, args);
+            }
+
+            throw new ArgumentException();
         }
 
         private class NamespaceFieldInfo : FieldInfo
         {
-            // public NamespaceFieldInfo
-
-            public override RuntimeFieldHandle FieldHandle => throw new NotImplementedException();
-
-            public override Type FieldType => typeof();
-
-            public override FieldAttributes Attributes => throw new NotImplementedException();
-
-            public override string Name => throw new NotImplementedException();
-
-            public override Type DeclaringType => throw new NotImplementedException();
-
-            public override Type ReflectedType => throw new NotImplementedException();
-
-            public override object[] GetCustomAttributes(bool inherit)
+            string _Name;
+            public NamespaceFieldInfo(string name)
             {
-                throw new NotImplementedException();
+                this._Name = name;
             }
 
-            public override object[] GetCustomAttributes(Type attributeType, bool inherit)
-            {
-                throw new NotImplementedException();
-            }
+            public override Type FieldType => typeof(NamespaceProxy);
+
+            public override FieldAttributes Attributes => FieldAttributes.Public;
+
+            public override string Name => _Name;
+
+            public override Type DeclaringType => typeof(NamespaceProxy);
+
+            public override Type ReflectedType => typeof(NamespaceProxy);
 
             public override object GetValue(object obj)
             {
-                throw new NotImplementedException();
+                NamespaceProxy parent = obj as NamespaceProxy;
+                return new NamespaceProxy($"{parent.Namespace}.{Name}");
             }
 
-            public override bool IsDefined(Type attributeType, bool inherit)
-            {
-                throw new NotImplementedException();
-            }
+            #region Attributes
+
+            public override object[] GetCustomAttributes(bool inherit) => new object[] { };
+
+            public override object[] GetCustomAttributes(Type attributeType, bool inherit) => new object[] { };
+
+            public override bool IsDefined(Type attributeType, bool inherit) => false;
+            #endregion
+
+            public override RuntimeFieldHandle FieldHandle => throw new NotImplementedException();
 
             public override void SetValue(object obj, object value, BindingFlags invokeAttr, Binder binder, CultureInfo culture)
             {
                 throw new NotImplementedException();
             }
         }
+        private class ObjectConstructorMethodInfo : MethodInfo
+        {
+            public Type BaseType { get; private set; }
+            public ConstructorInfo Constructor { get; private set; }
+
+            public ObjectConstructorMethodInfo(Type baseType, ConstructorInfo constructor)
+            {
+                this.BaseType = baseType;
+                this.Constructor = constructor;
+            }
+
+            public override MethodAttributes Attributes => MethodAttributes.Public;
+
+            public override string Name => BaseType.Name;
+
+            public override Type DeclaringType => typeof(NamespaceProxy);
+
+            public override Type ReflectedType => typeof(NamespaceProxy);
+
+
+            public override ParameterInfo[] GetParameters() => new ParameterInfo[] { };
+
+            public override object Invoke(object obj, BindingFlags invokeAttr, Binder binder, object[] parameters, CultureInfo culture)
+            {
+                object instance = Activator.CreateInstance(BaseType, parameters);
+
+                IInitializeWithWindow castedObject = instance as IInitializeWithWindow;
+                if (castedObject != null)
+                {
+                    castedObject.Initialize(FormsApplication.OpenForms[0].Handle);
+                }
+
+                return new ObjectProxy(instance);
+            }
+
+            public override MethodImplAttributes GetMethodImplementationFlags() => MethodImplAttributes.Runtime;
+
+            #region Attributes
+            public override object[] GetCustomAttributes(bool inherit) => new object[] { };
+
+            public override object[] GetCustomAttributes(Type attributeType, bool inherit) => new object[] { };
+
+            public override bool IsDefined(Type attributeType, bool inherit) => false;
+            #endregion
+
+            #region NotImplemented
+            public override MethodInfo GetBaseDefinition()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override ICustomAttributeProvider ReturnTypeCustomAttributes => throw new NotImplementedException();
+
+            public override RuntimeMethodHandle MethodHandle => throw new NotImplementedException();
+            #endregion
+        }
     }
 
+    /// <summary>
+    /// Exposes members of an WinRT or .Net object via IDispatch COM
+    /// </summary>
     [ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
     public class ObjectProxy : IReflect
     {
 
-        public object BaseObject { get; private set; };
+        public object BaseObject { get; private set; }
 
         public ObjectProxy(object obj)
         {
@@ -213,7 +353,10 @@ namespace MC_Studio
                 switch (member.MemberType)
                 {
                     case MemberTypes.Method:
-                        retval = NewLateBinding.LateCall(BaseObject, null, name, args, null, null, null, false);
+                        if (!invokeAttr.HasFlag(BindingFlags.InvokeMethod))
+                            throw new MissingMethodException();
+                        else
+                            retval = NewLateBinding.LateCall(BaseObject, null, name, args, null, null, null, false);
                         break;
                     case MemberTypes.Property:
                         if (invokeAttr.HasFlag(BindingFlags.GetProperty))
@@ -225,7 +368,8 @@ namespace MC_Studio
                         throw new NotImplementedException();
                 }
             }
-            if (retval.GetType().IsPrimitive || retval.GetType() == typeof(string))
+
+            if (retval == null || retval.GetType().IsPrimitive || retval.GetType() == typeof(string))
                 return retval;
             else
                 return new ObjectProxy(retval);
